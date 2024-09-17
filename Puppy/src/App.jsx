853c377ae167d95150c3d91b6e792ad4e7e8ad9f
avithew <avithew@gmail.com>
2024-09-16 19:04:49 -0600

    Select a Featured Puppy

--- Puppy/src/App.jsx
+++ Puppy/src/App.jsx
@@ -4,12 +4,33 @@ import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  function onClick() {}
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       <div>
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
       </div>
     </>
